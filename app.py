6fa87ec9d746ce28fe5bc69ef12510f12225f3aa
MadhuSri1 <93851919+MadhuSri1@users.noreply.github.com>
2021-11-20 20:12:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,34 @@
-import numpy as np
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 import pickle
+import numpy as np
 
-app = Flask(__name__)
-filename = 'diabetes-prediction.pkl'
+filename = 'classifier.pkl'
 classifier = pickle.load(open(filename, 'rb'))
 
+app = Flask(__name__)
+
 @app.route('/')
+
 def home():
-    return render_template('ditect.html')
-
-@app.route('/predict',methods=['POST'])
-def predict():
     
-    float_features = [float(X) for X in request.form.values()]
-    final_features = [np.array(float_features)]
-    pred = model.predict( scaler.transform(final_features) )
-    return render_template('result.html', prediction = pred)
+    return render_template('ditect.html')  
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    if request.method == 'POST':
+        preg = int(request.form['pregnancies'])
+        glucose = int(request.form['glucose'])
+        bp = int(request.form['bloodpressure'])
+        st = int(request.form['skinthickness'])
+        insulin = int(request.form['insulin'])
+        bmi = float(request.form['bmi'])
+        dpf = float(request.form['dpf'])
+        age = int(request.form['age'])
+        
+        data = np.array([[preg, glucose, bp, st, insulin, bmi, dpf, age]])
+        my_prediction = classifier.predict(data)
+        
+        return render_template('result.html', prediction=my_prediction)
+
+if __name__ == '__main__':
+	app.run(debug=True)
