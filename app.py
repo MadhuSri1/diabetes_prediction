c6b774e3c2ab72fb4a639935fac4298f3f730263
MadhuSri1 <93851919+MadhuSri1@users.noreply.github.com>
2021-11-21 16:51:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
+
+sentry_sdk.init('YOUR_DSN_HERE', integrations=[FlaskIntegration()])
 import pickle
 import numpy as np
 
-sentry_sdk.init('YOUR_DSN_HERE', integrations=[FlaskIntegration()])
+
 
 filename = 'classifier.pkl'
 classifier = pickle.load(open(filename, 'rb'))
