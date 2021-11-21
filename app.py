fee772050290573e5e56dea5e10656392b6490a6
MadhuSri1 <93851919+MadhuSri1@users.noreply.github.com>
2021-11-21 16:52:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ filename = 'classifier.pkl'
 classifier = pickle.load(open(filename, 'rb'))
 
 app = Flask(__name__)
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 @app.route('/')
 
@@ -35,9 +38,7 @@ def predict():
         pred = classifier.predict(data)
         
         return render_template('result.html', prediction=pred)
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
