871a262cd26c2be33412cb28b15c9f873cbc18ea
MadhuSri1 <93851919+MadhuSri1@users.noreply.github.com>
2021-11-21 10:57:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
-import logging
+
 filename = 'classifier.pkl'
 classifier = pickle.load(open(filename, 'rb'))
 
 app = Flask(__name__)
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
 
 @app.route('/')
 
